4ef34acaf2efeec999d9b7467b797d41ed189fd8
arafataft <arafatcd@gmail.com>
2023-08-10 15:19:14 +0600

    customer section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Reviews from "../../Components/Review";
 import Contact from "../../Components/Contact";
 import Team from "../../Components/Team";
+import Customer from "./Customer/Customer";
 
 const Home = () => {
   return (
@@ -16,6 +17,8 @@ const Home = () => {
       <button className="btn btn-active btn-link">Link</button>
       <Link to={'/login'}><button className="btn btn-active btn-primary">Login</button></Link>
       <Link to={'/register'}><button className="btn btn-active btn-secondary">Register</button></Link> 
+
+      <Customer></Customer>
 
       <Reviews></Reviews> 
       <Contact></Contact>
