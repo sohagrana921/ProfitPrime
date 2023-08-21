5c14f5248eeca1552d59034735f3e99705518ff1
Sohag Rana <sohagrana921@gmail.com>
2023-08-21 19:09:46 +0600

    env file public for work

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_appId,
+  apiKey: "AIzaSyBJxNeCmZgfTNg1_Sl_9GzCpP6LE9klok4",
+  authDomain: "profit-prime.firebaseapp.com",
+  projectId: "profit-prime",
+  storageBucket: "profit-prime.appspot.com",
+  messagingSenderId: "901408189518",
+  appId: "1:901408189518:web:0c89db7556e79e336ca71f",
+  // apiKey: import.meta.env.VITE_apiKey,
+  // authDomain: import.meta.env.VITE_authDomain,
+  // projectId: import.meta.env.VITE_projectId,
+  // storageBucket: import.meta.env.VITE_storageBucket,
+  // messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  // appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
