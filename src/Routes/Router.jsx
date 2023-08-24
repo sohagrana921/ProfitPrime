98046366aae3c60809e74273829701aeefcd9225
SalehinShohan <ps.shohan1@gmail.com>
2023-08-24 23:01:00 +0600

    fixed navbar and add customer sub section

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -6,13 +6,8 @@ import Register from "../pages/Login&Register/Register";
 import DataAnalysis from "../pages/Products/DataAnalysis";
 import ConsultingProducts from "../pages/Home/ConsultingProducts/ConsultingProducts";
 import CustomerService from "../pages/Home/CustomerService/CustomerService";
-import BussinessOption from "../pages/Products/BussinessOption";
-import CompetitorSolution from "../pages/Products/CompetitorSolution";
-import Pricing from "../Components/Pricing";
-import ResetPassword from "../pages/ResetPassword/ResetPassword";
-import Dashboard from "../Dashboard/Dashboard/Dashboard"
-import DashHome from "../Dashboard/DashHome/DashHome"
-import Payment from "../pages/Payment/Payment";
+import UpscaleCustomer from "../pages/Home/UpscaleCustomer/UpscaleCustomer";
+import HybridCustomer from "../pages/Home/HybridCustomer/HybridCustomer";
 
 const router = createBrowserRouter([
   {
@@ -29,22 +24,6 @@ const router = createBrowserRouter([
         element: <DataAnalysis></DataAnalysis>,
       },
       {
-        path: "/businessoption",
-        element: <BussinessOption />,
-      },
-      {
-        path: "/pricing",
-        element: <Pricing />,
-      },
-      {
-        path: "/payment/:plan/:billingOption",
-        element: <Payment/>,
-      },
-      {
-        path: "/competitor",
-        element: <CompetitorSolution />,
-      },
-      {
         path: "/login",
         element: <Login></Login>,
       },
@@ -53,28 +32,23 @@ const router = createBrowserRouter([
         element: <Register></Register>,
       },
       {
-        path: "/resetPassword",
-        element: <ResetPassword></ResetPassword>,
+        path: '/consulting',
+        element: <ConsultingProducts></ConsultingProducts>
       },
       {
-        path: "/consulting",
-        element: <ConsultingProducts></ConsultingProducts>,
+        path: '/customerService',
+        element: <CustomerService></CustomerService>
       },
       {
-        path: "/customerService",
-        element: <CustomerService></CustomerService>,
+        path: '/upscale',
+        element: <UpscaleCustomer></UpscaleCustomer>
       },
+      {
+        path: '/hybridcustomer',
+        element: <HybridCustomer></HybridCustomer>
+      }
     ],
   },
-  {
-    path: "/dashboard",
-    element: <Dashboard></Dashboard>,
-    children: [
-      {
-        path: "dashhome",
-        element: <DashHome></DashHome>
-      }
-    ]
-  }
 ]);
 export default router;
+
