0f569771caf36279c89e1073e51fde8963543241
Sohag Rana <sohagrana921@gmail.com>
2023-08-25 19:51:20 +0600

    date issue fixed

--- src/Dashboard/DashHome/DashHome.jsx
+++ src/Dashboard/DashHome/DashHome.jsx
@@ -1,17 +1,17 @@
 import SubscriptionCard from "./SubscriptionCard";
 
 const DashHome = () => {
-    return (
-        <div className="flex justify-center w-full items-center h-screen bg-green-100">
+  return (
+    <div className="flex justify-center w-full items-center h-screen bg-green-100">
       <SubscriptionCard
         companyName="AB Company LTD."
         planName="Prime Plan"
-        subscriptionDate="Sep 25, 2023"
-        endDate="Sep 24, 2024"
+        subscriptionDate="Aug 25, 2023"
+        endDate="Aug 24, 2024"
         imageUrl="https://via.placeholder.com/150"
       />
     </div>
-    );
+  );
 };
 
 export default DashHome;
