bebc0d2a86654806d3a349cb43ecf11c3ed47d48
Md. Ashiqur Rahman Tonmoy <clans5780@gmail.com>
2023-08-28 17:43:23 +0600

    Admin Dashboard Added

---
+++ src/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -0,0 +1,11 @@
+
+const AdminHome = () => {
+
+    return (
+        <div>
+            <h1 className="text-4xl font-bold">Welcome To Admin Dashboard</h1>
+        </div>
+    );
+};
+
+export default AdminHome;
