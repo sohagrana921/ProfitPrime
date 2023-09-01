6fdbc9c0420a423069849eadea952a088de4bf9b
arafataft <arafatcd@gmail.com>
2023-09-01 20:59:38 +0600

    update

--- src/Dashboard/DashHome/DashHome.jsx
+++ src/Dashboard/DashHome/DashHome.jsx
@@ -1,13 +1,34 @@
+import { useContext } from "react";
 import SubscriptionCard from "./SubscriptionCard";
+import { AuthContext } from "../../providers/AuthProvider";
+import { useEffect } from "react";
+import axios from "axios";
+import { useState } from "react";
 
 const DashHome = () => {
+  const {user}=useContext(AuthContext);
+  const email =user?.email;
+  console.log(email);
+  const [customer, setCustomer] = useState();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:5000//user/${email}`)
+      .then((response) => {
+        setCustomer(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching customer information:", error);
+      });
+  }, [email]);
+  console.log(customer);
+
   return (
     <div className="flex justify-center w-full items-center h-screen bg-green-100">
       <SubscriptionCard
-        companyName="AB Company LTD."
-        planName="Prime Plan"
-        subscriptionDate="Aug 25, 2023"
-        endDate="Aug 24, 2024"
+        companyName={customer?.name} 
+        planName={customer?.userRole}
+        subscriptionDate="Sep 1, 2023"
+        endDate=""
         imageUrl="https://via.placeholder.com/150"
       />
     </div>
--- src/Dashboard/DashHome/SubscriptionCard.jsx
+++ src/Dashboard/DashHome/SubscriptionCard.jsx
@@ -6,15 +6,22 @@ const SubscriptionCard = ({
   subscriptionDate,
   endDate,
 }) => {
+
+  if(planName=='Prime'){
+    endDate='1 sept 2024'
+  }else{
+    endDate='1 oct 2023'
+  }
   return (
     <div className="flex w-full flex-col items-center justify-center min-h-screen bg-base-100">
       <div className="absolute top-0 right-0 p-4 text-gray-700">
-        <Link>{companyName}</Link>
+        <Link>{companyName?companyName:'XY'} Company LTD</Link>
       </div>
       <div className="flex flex-col items-center justify-center bg-green-700 text-white p-8 rounded-lg shadow-lg max-w-md w-full">
-        <h2 className="text-xl font-semibold mb-2">{planName}</h2>
+        <h2 className="text-xl font-semibold mb-2">{planName} Plan</h2>
         <p className="text-white mb-2">
-          {subscriptionDate} - {endDate}
+           
+           <span> {subscriptionDate}  -{endDate}</span>
         </p>
       </div>
       {/* <img src={imageUrl} alt="Subscription" className="w-500 h-full" /> */}
