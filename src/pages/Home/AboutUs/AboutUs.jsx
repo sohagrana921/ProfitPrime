552d9627c1efeb4994e03cba3de7218b2d5a10d7
arafataft <arafatcd@gmail.com>
2023-09-08 13:02:02 +0600

    fix all button

--- src/pages/Home/AboutUs/AboutUs.jsx
+++ src/pages/Home/AboutUs/AboutUs.jsx
@@ -1,17 +1,36 @@
+import { useState } from 'react';
+
 const AboutUs = () => {
+  const [showFullText, setShowFullText] = useState(false);
+
+  const toggleFullText = () => {
+    setShowFullText(!showFullText);
+  };
+
   return (
-    <div className="flex justify-center items-center my-container mt-24">
+    <div id="about" className="flex justify-center items-center my-container mt-24">
       <div className="container mx-auto md:flex items-center">
         <div className="md:w-1/2 px-5 md:ml-10 mt-5 md:mt-0">
           <h2 className="text-3xl font-semibold mb-4">About Us</h2>
           <p className="text-gray-700 mb-6 text-justify">
-            Our Company Revenue Generation Platform website project is your gateway to enhanced growth and prosperity. Through our innovative products, transparent pricing, and powerful features, we empower your business to easily optimize revenue strategies. Embrace technologys potential to streamline operations, boost customer engagement, and amplify profits. This marks a new chapter in revenue generation, propelling your business towards efficiency, competitiveness, and lasting triumph. Welcome to the future of revenue excellence. Maximize profits effortlessly with our Revenue Generation Platform. Harness data-driven insights, optimize strategies, and elevate customer engagement for exponential growth.
+            {showFullText
+              ? "Our Company Revenue Generation Platform website project is your gateway to enhanced growth and prosperity. Through our innovative products, transparent pricing, and powerful features, we empower your business to easily optimize revenue strategies. Embrace technology's potential to streamline operations, boost customer engagement, and amplify profits. This marks a new chapter in revenue generation, propelling your business towards efficiency, competitiveness, and lasting triumph. Welcome to the future of revenue excellence. Maximize profits effortlessly with our Revenue Generation Platform. Harness data-driven insights, optimize strategies, and elevate customer engagement for exponential growth."
+              : "Our Company Revenue Generation Platform website project is your gateway to enhanced growth and prosperity. Through our innovative products, transparent pricing, and powerful features, we empower your business to easily optimize revenue strategies."}
           </p>
+          
           <div className="flex space-x-4">
-            <button className="px-4 py-2 bg-green-700 text-white rounded hover:bg-green-900 transition duration-300">
+          {!showFullText && (
+            <button
+              onClick={toggleFullText}
+              className="px-4 py-2 bg-green-700 text-white rounded hover:bg-green-900 transition duration-300"
+            >
               Learn More
             </button>
-            <a href="#contact" className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 transition duration-300">
+          )}
+            <a
+              href="#contact"
+              className="px-4 py-2 bg-gray-300 text-gray-700 rounded hover:bg-gray-400 transition duration-300"
+            >
               Contact Us
             </a>
           </div>
