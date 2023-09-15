fde53c36202384d0150dc0f146ccb41883b5cd53
Sohag Rana <sohagrana921@gmail.com>
2023-09-15 10:46:12 +0600

    Section titile issue solved

--- src/pages/Shared/SectionTitle/SectionTitle.jsx
+++ src/pages/Shared/SectionTitle/SectionTitle.jsx
@@ -1,8 +1,8 @@
 const SectionTitle = ({ subheading, heading }) => {
   return (
-    <div className="text-center my-16 md:my-32 md:px-64">
-      <p className="text-red-800">---{subheading}---</p>
-      <h3 className="text-md lg:text-2xl font-bold border-purple-900 border-y-4 py-2 uppercase text-red-800">
+    <div className="mx-auto text-center w-10/12 my-12">
+      <p className="text-red-800">--- {subheading} ---</p>
+      <h3 className="text-sm lg:text-2xl font-bold border-purple-900 border-y-4 py-2 uppercase text-red-800">
         {heading}
       </h3>
     </div>
