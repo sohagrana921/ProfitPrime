f4704e1732873c9f2e2d5d9ce4dd9e03afb08c0a
SalehinShohan <ps.shohan1@gmail.com>
2023-09-19 22:20:25 +0600

    added count down

--- src/Dashboard/DashHome/SubscriptionCard.jsx
+++ src/Dashboard/DashHome/SubscriptionCard.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import anime from "../../../animation_lmiy89am.json";
 import Lottie from "lottie-react";
+import { differenceInSeconds } from "date-fns";
 
 const SubscriptionCard = ({
   companyName,
@@ -8,11 +10,44 @@ const SubscriptionCard = ({
   subscriptionDate,
   endDate,
 }) => {
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  // Calculate the remaining time and update it every second
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const endDateParsed = new Date(endDate);
+      const today = new Date();
+      const timeRemaining = differenceInSeconds(endDateParsed, today);
+
+      if (timeRemaining <= 0) {
+        clearInterval(interval); // Stop the countdown when it reaches zero
+        setCountdown({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      } else {
+        const days = Math.floor(timeRemaining / (60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (60 * 60 * 24)) / (60 * 60));
+        const minutes = Math.floor((timeRemaining % (60 * 60)) / 60);
+        const seconds = timeRemaining % 60;
+
+        setCountdown({ days, hours, minutes, seconds });
+      }
+    }, 1000); // Update every second
+
+    return () => {
+      clearInterval(interval); // Clean up the interval when the component unmounts
+    };
+  }, [endDate]);
+
   if (planName == "Prime") {
     endDate = "15 Sept 2024";
   } else {
     endDate = "15 Oct 2023";
   }
+
   return (
     <div className="flex w-full flex-col items-center justify-center min-h-screen bg-base-100">
       <div className="absolute top-0 right-0 p-4 text-gray-700">
@@ -30,13 +65,15 @@ const SubscriptionCard = ({
           </h2>
           <p className="mb-2 text-purple-950">
             <span>
-              {" "}
-              {subscriptionDate} -{endDate}
+              {subscriptionDate} - {endDate}
             </span>
+          </p>
+          <p className="mb-2">
+            Time Remaining: {countdown.days} days, {countdown.hours} hours,{" "}
+            {countdown.minutes} minutes, {countdown.seconds} seconds
           </p>
         </div>
       </div>
-      {/* <img src={imageUrl} alt="Subscription" className="w-500 h-full" /> */}
     </div>
   );
 };
