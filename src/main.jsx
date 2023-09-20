303ec11bcf625ec462c9c7b2eb01eb731314f7c5
arafataft <arafatcd@gmail.com>
2023-09-20 23:44:13 +0600

    redux store setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,15 +6,19 @@ import AuthProvider from "./providers/AuthProvider";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HelmetProvider } from "react-helmet-async";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
+        <Provider store={store}>
         <div className="max-w-screen-xl mx-auto font-serif">
           <RouterProvider router={router} />
         </div>
+        </Provider>
       </QueryClientProvider>
     </HelmetProvider>
   </AuthProvider>
